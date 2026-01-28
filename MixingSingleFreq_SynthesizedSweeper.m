27b3922bdc62cb8d6b1bd9b64bb5167755174c85
nrenaud12 <114703460+nrenaud12@users.noreply.github.com>
2026-01-27 16:47:08 -0800

    Add files via upload

---
+++ MixingSingleFreq_SynthesizedSweeper.m
@@ -0,0 +1,44 @@
+% Define parameters
+gpibAddress = 15; 
+frequency_GHz = 5;
+power_dBm = -10;
+
+resourceName = sprintf('GPIB0::%d::INSTR', gpibAddress);
+sigGen = visadev(resourceName);
+
+try
+    % 1. Preset the instrument
+    writeline(sigGen, 'IP'); 
+    pause(0.5);
+    
+    % 2. SET FREQUENCY (CW mode)
+    writeline(sigGen, sprintf('CW%fGZ', frequency_GHz));
+    
+    % 3. SET POWER LEVEL
+    writeline(sigGen, sprintf('PL%fDB', power_dBm));
+    
+    % 4. ENABLE RF OUTPUT
+    writeline(sigGen, 'RF1'); 
+    
+    fprintf('Signal Generator set to %g GHz at %g dBm. RF is ON.\n', ...
+            frequency_GHz, power_dBm);
+
+    % ---- Do whatever measurement you want here ----
+     pause(2);  % example: leave it on for 2 seconds
+
+catch ME
+    warning('Error: %s', ME.message);
+end
+
+% 5. TURN OFF RF OUTPUT (always try, even if an error occurred above)
+try
+    writeline(sigGen, 'RF0');     % RF OFF
+    % Optional: return to local control (may be ignored depending on model)
+    % writeline(sigGen, 'LOCAL');
+    fprintf('RF is OFF.\n');
+catch ME2
+    warning('Could not turn RF OFF: %s', ME2.message);
+end
+
+clear sigGen;
+
